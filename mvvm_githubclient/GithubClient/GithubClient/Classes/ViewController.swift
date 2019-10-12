7cba7488e9a127c45d31966e5deb983c36ea4602
Kazuya Hatano <k28@me.com>
2019-10-13 07:05:41 +0900

    Add GithubClient project

---
+++ mvvm_githubclient/GithubClient/GithubClient/Classes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubClient
+//
+//  Created by kazuya on 2019/10/13.
+//  Copyright © 2019 K.Hatano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
